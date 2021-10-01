2ddb687bf553d8980bd0a0825d6d4d40d07fbfc5
Xuwei Li <dev@xuwei.li>
2021-10-01 01:08:17 +0000

    Fix fontawesome CDN link in react components.

--- components/search-bar.js
+++ components/search-bar.js
@@ -53,7 +53,7 @@ export default class SearchBar extends React.Component {
     return(
       <div id="search-bar" className={styles['search-bar']}>
         <Head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
         </Head>
 
 
